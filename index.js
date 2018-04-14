f438e64e4cc75357f724e77468fb581961e0dadb
Joey Vinyard <jviny98@yahoo.com>
2018-04-14 18:59:08 -0400

    Routes for login and signup made

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/assets/index.html");
+})
+
+app.get('/login', (req, res) => {
+	res.sendFile(__dirname + "/assets/routes/login.html");
+});
+
+app.get("/signup", (req, res) => {
+	res.sendFile(__dirname + "/assets/routes/signup.html");
+})
+
+app.post('/login', (req, res) => {
+	console.log(req.body);
+});
+
+app.post("/signup", (req, res) => {
+	console.log(req.body);
 })
 
 app.get('/*.*', (req, res) => {
