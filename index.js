821f5849ef57d1e9e0e33851f83cbd9e337f9231
calvinhenry <calvinhenry60@gmail.com>
2018-04-14 15:51:05 -0400

    Started database

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express')
 const app = express()
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var db;
+
+MongoClient.connect(url, function(err, dbo) {
+  if (err) throw err;
+  db = dbo.db("SliceLineDB");
+  console.log("Database loaded");
+  createUser({username: "calvin", password: "123456"});
+});
+
+function createUser(userObject){
+	if(!userObject.username || !userObject.password){
+		return -1;
+	}
+	db.collection("users").insertOne(userObject, function(err, res) {
+    if (err) throw err;
+	console.log("User Created");
+	return 0;
+  });
+}
+
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/assets/index.html");
@@ -10,5 +32,7 @@ app.get('/*.*', (req, res) => {
 })
 
 app.listen(3000, () => {
-
+	console.log("Listening on 3000");
 });
+
+//
