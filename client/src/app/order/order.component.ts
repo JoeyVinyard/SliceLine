c5e19c1f4eeb47936cbe60f4c1701e1a3830434f
Cameron Glass <glass10@purdue.edu>
2018-04-22 00:32:22 -0400

    Order Page Finished

--- client/src/app/order/order.component.ts
+++ client/src/app/order/order.component.ts
@@ -7,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  selectedStore = "";
+  orderCat = "";
+  pizzaCrust = "";
+  pizzaSize = "";
+
+  testArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+  setStore(number){
+    this.selectedStore = "";
+
+    //call menu from here
+  }
+  setCat(category){
+    this.orderCat = category;
+    console.log("Category: " + this.orderCat)
+
+    if(category != "pizza"){
+      this.pizzaCrust = "";
+      this.pizzaSize = "";
+    }
+  }
+
+  setCrust(crust){
+    this.pizzaCrust = crust;
+    console.log("Crust: " + this.pizzaCrust)
+
+    //get all items with FlavorCode from Varients here
+  }
+
+  setSize(size){
+    this.pizzaSize = size;
+    console.log("Size: " + this.pizzaSize)
+
+    // filter results based on SizeCode
+  }
+
   constructor() { }
 
   ngOnInit() {
