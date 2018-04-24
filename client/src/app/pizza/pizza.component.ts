cc781be8193ab579881db5fcd3285e57db6137c5
Cameron Glass <glass10@purdue.edu>
2018-04-24 11:46:28 -0400

    Pizza page reformatted

--- client/src/app/pizza/pizza.component.ts
+++ client/src/app/pizza/pizza.component.ts
@@ -14,6 +14,7 @@ export class PizzaComponent implements OnInit {
 
 	testArray=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 	parties = [];
+	
 	sort(criteria, direction){
 		if(criteria == "distance"){
 			if(direction == "up"){
